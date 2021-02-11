9c47d9616aab108d27321dfdf1b5054f781161ae
Jrumfelt <jrumfelt1213@gmail.com>
2021-02-11 10:45:03 -0500

    Added signup window

--- Overtime.py
+++ Overtime.py
@@ -5,6 +5,7 @@ Email: jrumfelt1213@gmail.com
 Phone: (518)414-1483
 Purpose: Determine Overtime position priority for Schenectady PD
 """
+from os import close
 import sys
 from csv import *
 from PyQt5.QtWidgets import *
@@ -15,6 +16,7 @@ from tempfile import NamedTemporaryFile
 
 fname = "Names.csv"
 fields = ["id", "name", "job", "8hours", "4hours", "overtimerank", "previousposition"]
+unranked = []
 
 """
 Opens csv file in append mode and adds a new employee using file and writer object
@@ -173,7 +175,7 @@ def shiftprevious(prevrow):
     tempfile = NamedTemporaryFile(mode="w", delete=False, newline="")
     count = 0
     if prevrow["previousposition"] == "":
-        print("Error: Employee has not been assigned a previous position")
+        return False
     with open(fname, "r") as csvfile, tempfile:
         reader = DictReader(csvfile, fieldnames=fields)
         writer = DictWriter(tempfile, fieldnames=fields)
@@ -184,6 +186,7 @@ def shiftprevious(prevrow):
                     writer.writerow(prevrow)
                 writer.writerow(row)
     shutil.move(tempfile.name, fname)
+    return True
     
 """
 Confirm employee up for overtime
@@ -210,16 +213,111 @@ Cancel overtime for a employee who has been signed up
 """
 def cancelovertime(id):
     dictall = viewall()
-    employee = dictall[id]
-    shiftprevious(employee)
+    if id in dictall:
+        employee = dictall[id]
+        return(shiftprevious(employee))
+    else:
+        return False
 
 """
 GUI classes and methods
 """
 
 """
-Table of employees
+Table of employees signed up for overtime but not ranked
 """
+class UnrankedTable(QTableWidget):
+    def __init__(self, r, c):
+        super().__init__(r, c)
+        self.check_change = True
+        self.init_ui()
+        
+    def init_ui(self):
+        self.cellChanged.connect(self.c_current)
+        self.show()
+        
+    def c_current(self):
+        if self.check_change:
+            row = self.currentRow()
+            col = self.currentColumn()
+            value = self.item(row, col)
+            value = value.text()
+            
+    def read_list(self):
+        self.check_change = False
+        self.setRowCount(0)
+        self.setColumnCount(3)
+        column = 0
+        for row_data in unranked:
+            row = self.rowCount()
+            self.insertRow(row)
+            for col_data in row_data: 
+                item = QTableWidgetItem(col_data)
+                self.setItem(row, column, item)
+                column += 1
+            column = 0
+                
+"""
+Window with table of signed up employees and form allowing user to add employees to table
+""" 
+class SignUp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Assign Overtime")
+        self.setWindowIcon(QtGui.QIcon("Icon"))
+        self.resize(450, 400)
+        
+        #create menu bar
+        assignbar = self.menuBar()
+        
+        #Create actions for menu buttons
+        add_action = QAction("Add Employee", self)
+        calc_action = QAction("Calculate Overtime", self)
+        close_action = QAction("Close", self)
+        
+        #Add menu buttons to menu bar
+        assignbar.addAction(add_action)
+        assignbar.addAction(calc_action)
+        assignbar.addAction(close_action)
+        
+        #Connect menu buttons to functions
+        add_action.triggered.connect(self.add_triggered)
+        calc_action.triggered.connect(self.calc_triggered)
+        close_action.triggered.connect(self.close_triggered)
+        
+        #set up table
+        self.form_widget = UnrankedTable(3,3)
+        self.setCentralWidget(self.form_widget)
+        self.form_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        
+        headers = ["ID", "8 Hour", "4 Hour"]
+        self.form_widget.setHorizontalHeaderLabels(headers)
+        
+        self.form_widget.read_list()
+        
+    #Methods for when you press menu button
+    """
+    Add user to list and update form_widget table
+    """
+    def add_triggered(self):
+        return False
+    
+    """
+    Calculate rank order with rank() and display new window where the user can assign employees to overtime
+    """
+    def calc_triggered(self):
+        return False
+        
+    """
+    Close window
+    """
+    def close_triggered(self):
+        self.close()
+
+"""
+Table of employees
+"""     
 class EmployeeTable(QTableWidget):
     def __init__(self, r, c):
         super().__init__(r, c)
@@ -247,12 +345,11 @@ class EmployeeTable(QTableWidget):
             for row_data in my_file:
                 row = self.rowCount()
                 self.insertRow(row)
-                if len(row_data) > 10:
-                    self.setColumnCount(len(row_data))
                 for column, stuff in enumerate(row_data):
                     item = QTableWidgetItem(stuff)
                     self.setItem(row, column, item)
-        self.check_change = True           
+        self.check_change = True    
+               
 """
 First window with list of all employees and menu bar with buttons to calculate overtime rank, cancel overtime, add new employe, and quit application
 """     
@@ -268,7 +365,7 @@ class HomeWindow(QMainWindow):
         bar = self.menuBar()
         
         #Create actions for menu buttons
-        rank_action = QAction("Calculate Overtime", self)    
+        rank_action = QAction("Schedule Overtime", self)    
         cancel_action = QAction("Cancel Overtime", self)
         newemp_action = QAction("Add New Employee", self)
         quit_action = QAction("Quit", self)
@@ -288,6 +385,8 @@ class HomeWindow(QMainWindow):
         #set up table
         self.form_widget = EmployeeTable(10, 10)
         self.setCentralWidget(self.form_widget)
+        self.form_widget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        
             
         headers = ["ID", "Name", "Position"]
         self.form_widget.setHorizontalHeaderLabels(headers)
@@ -302,9 +401,8 @@ class HomeWindow(QMainWindow):
         qApp.quit()
     
     def rank_triggered(self):
-        print("Rank")
-    
-    
+        signup.show()
+        
     #Methods for getting employee information
     def getUID(self):
         uid, okPressed = QInputDialog.getText(self, "Get ID","Employee ID", QLineEdit.Normal, "")
@@ -332,7 +430,7 @@ class HomeWindow(QMainWindow):
     Prompt user to confirm and if confirmed prompt if the user would like to select a new user for overtime
     """    
     def cancel_triggered(self):
-        uid = self.getuid()
+        uid = self.getUID()
         if uid:
             msgBox = QMessageBox()
             msgBox.setText("Confirm Employee Information Is Correct\n____________________________________________\n\nEmployee ID:    " + uid)
@@ -341,10 +439,31 @@ class HomeWindow(QMainWindow):
             msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
                     
             confirm = msgBox.exec_()
+            
             if confirm == QMessageBox.Ok:
-                cancelovertime(uid)
-                #TODO: ASK FOR NEW EMPLOYEE TO REPLACE THIS ONE
-   
+                success = cancelovertime(uid)
+                print(success)
+                if success == False:
+                    msgBox = QMessageBox()
+                    msgBox.setText("Error: Employee overtime could not be cancelled.\nPlease check if employee information is correct and if they were signed up for overtime.")
+                    msgBox.setWindowIcon(QtGui.QIcon("Icon"))
+                    msgBox.setWindowTitle("Error")
+                    msgBox.setStandardButtons(QMessageBox.Ok)
+                    msgBox.exec_()
+                else:
+                    #ask if they want to assign someone new
+                    msgBox = QMessageBox()
+                    msgBox.setText("Assign New Employee to Overtime?")
+                    msgBox.setWindowIcon(QtGui.QIcon("Icon"))
+                    msgBox.setWindowTitle("Confirmation")
+                    msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+                    
+                    confirm = msgBox.exec_()
+                    
+                    if confirm == QMessageBox.Ok:
+                        signup.show()
+                        #Change assign to other window
+
     """
     When new employee button is clicked prompt the user to enter the id, name, and position of employee
     On user confirmation that the information is correct add to Names.csv using newemployee() function
@@ -367,9 +486,11 @@ class HomeWindow(QMainWindow):
                     confirm = msgBox.exec_()
                     if confirm == QMessageBox.Ok:
                         newemployee(uid, name, position)
-        
+        self.form_widget.open_sheet()    
+                
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     home = HomeWindow()
+    signup = SignUp()
     sys.exit(app.exec_())
     
